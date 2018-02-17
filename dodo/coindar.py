555f6cdac69ffdd6556f6466b315233884565897
Peerchemist <dx.1178@gmail.com>
2018-02-17 12:55:16 +0100

    implement filtering of past events

--- dodo/coindar.py
+++ dodo/coindar.py
@@ -3,6 +3,22 @@ from datetime import datetime
 import json
 
 
+def to_datetime(string: str) -> datetime.strptime:
+    '''convert string to datetime'''
+
+    if len(string.split()) == 1:
+
+        if len(string.split('-')) == 2:
+            return datetime.strptime(string, '%Y-%m')
+
+        if len(string.split('-')) == 3:
+            return datetime.strptime(string, '%Y-%m-%d')
+
+    if len(string.split()) > 0:
+
+        return datetime.strptime(string, '%Y-%m-%d %H:%M')
+
+
 class Coindar:
 
     url = 'https://coindar.org/api/v1/'
@@ -17,24 +33,28 @@ class Coindar:
         return res
 
     @classmethod
-    def _filter_passed():
+    def _filter_passed(cls, res: list) -> list:
         '''filter events which have passed'''
-        raise NotImplementedError
+
+        return [i for i in res if datetime.now() <= to_datetime(i['start_date'])]
 
     @classmethod
-    def _fetch(cls, command):
+    def _fetch(cls, command: str) -> list:
 
         return json.loads(urlopen(cls.url + command).read())
 
     @classmethod
-    def query_coin_events(cls, coin):
+    def query_coin_events(cls, coin: str) -> list:
         '''query for events for <coin>'''
 
         res = cls._fetch('coinEvents?' + f'name={coin.lower()}')
-        return cls._filter_ru(res)
+        res = cls._filter_ru(res)
+        res = cls._filter_passed(res)
+
+        return res
 
     @classmethod
-    def query_events(cls, month, day=None):
+    def query_events(cls, month: int, day: int=None) -> list:
         '''query for events happening this month'''
 
         year = datetime.now().year
@@ -44,4 +64,7 @@ class Coindar:
             q += f'&day={day}'
 
         res = cls._fetch(q)
-        return cls._filter_ru(res)
+        res = cls._filter_ru(res)
+        res = cls._filter_passed(res)
+
+        return res
